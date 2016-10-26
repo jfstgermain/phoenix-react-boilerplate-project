13312540105e7c9a25fb89c9e40bb9be9e80d8af
Jean-François St-Germain <jfstgermain@gmail.com>
2016-10-26 15:41:44 -0400

    mod gitignore file

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,6 @@ module.exports = {
       // "es6": true,
       "browser": true
   },
-  "parserOptions": {
-      "ecmaVersion": 7,
-      "ecmaFeatures": {
-          "experimentalObjectRestSpread": true
-      }
-  },
   "rules": {
     "import/no-extraneous-dependencies": ["error", {
       "devDependencies": ["**/*.test.js", "**/*.spec.js", "**/webpack*.js"]
